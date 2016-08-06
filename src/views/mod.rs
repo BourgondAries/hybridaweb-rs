c0e2629e1c22bb88160476ca0b728cae55a01dfe
Bourgond Aries <macocio@gmail.com>
2016-08-06 23:56:43 +0200

    Make the navbar show the icon correctly

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -24,37 +24,39 @@ pub fn sur(custom_html: String) -> String {
 			body {
 				nav class="navbar navbar-default navbar-fixed-top" {
 					div class="container-fluid nav-container" {
-						button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation_menu" {
-							span class="sr-only" { "Toggle navigation" }
-							span class="icon-bar" { }
-							span class="icon-bar" { }
-							span class="icon-bar" { }
-						}
-						a class="navbar-brand" id="home" href="/" {
-							img src="/files/images/griff_drop_shadow.png" id="logo" alt="Griff logo" /
-						}
-					}
-					div class="collapse navbar-collapse" id="navigation_menu" {
-						ul class="nav navbar-nav" {
-							li { a href="/index" { "Nytt Innlegg" } }
-							li { a href="/bedrift" { "Bedrift" } }
-							li { a href="/about" { "Om Hybrida" } }
-							li { a href="/ringen" { "I&IKT-Ringen" } }
-						}
-						form class="navbar-form navbar-right" role="search" action="/search" method="GET" {
-							div class="form-group" {
-								input id="navbar-search" type="text" class="form-control" name="term" placeholder="Søk" /
+						div class="navbar-header" {
+							button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation_menu" {
+								span class="sr-only" { "Toggle navigation" }
+								span class="icon-bar" { }
+								span class="icon-bar" { }
+								span class="icon-bar" { }
+							}
+							a class="navbar-brand" id="home" href="/" {
+								img src="/files/images/griff_drop_shadow.png" id="logo" alt="Griff logo" /
 							}
 						}
-						ul class="nav navbar-nav navbar-right" {
-							li class="dropdown" {
-								a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#" { "UsernameXHD" }
-								ul class="dropdown-menu" role="menu" {
-									li { a href="userprof" { "Profil" }}
-									li { a href="rfid" { "RFID Prikking" }}
-									li { a href="bedrift" { "Bedrifter" }}
-									li { a href="logging" { "Logg ut" }}
-									li { a href="adminlogout" { "Logg ut" }}
+						div class="collapse navbar-collapse" id="navigation_menu" {
+							ul class="nav navbar-nav" {
+								li { a href="/index" { "Nytt Innlegg" } }
+								li { a href="/bedrift" { "Bedrift" } }
+								li { a href="/about" { "Om Hybrida" } }
+								li { a href="/ringen" { "I&IKT-Ringen" } }
+							}
+							form class="navbar-form navbar-right" role="search" action="/search" method="GET" {
+								div class="form-group" {
+									input id="navbar-search" type="text" class="form-control" name="term" placeholder="Søk" /
+								}
+							}
+							ul class="nav navbar-nav navbar-right" {
+								li class="dropdown" {
+									a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#" { "UsernameXHD" }
+									ul class="dropdown-menu" role="menu" {
+										li { a href="userprof" { "Profil" }}
+										li { a href="rfid" { "RFID Prikking" }}
+										li { a href="bedrift" { "Bedrifter" }}
+										li { a href="logging" { "Logg ut" }}
+										li { a href="adminlogout" { "Logg ut" }}
+									}
 								}
 							}
 						}
